8a38798790ccbe07716204d447133652aead54b4
Darstaed <Matt.dbker@gmail.com>
2015-08-24 15:36:25 +1000

    Update testmysql.php

--- testmysql.php
+++ testmysql.php
@@ -1,7 +1,7 @@
 <?php 
 $link = mysql_connect('hostname','dbuser','dbpassword'); 
 if (!$link) { 
-	die('Could not connect to ha: ' . mysql_error()); 
+	die('Could not connect to he: ' . mysql_error()); 
 } 
 echo 'Connection OK'; mysql_close($link); 
 ?> 
