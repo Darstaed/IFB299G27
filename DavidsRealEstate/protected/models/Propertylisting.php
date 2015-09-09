badc3a53e3391fe0daca7bb149141795c6d1c04d
Darstaed <Matt.dbker@gmail.com>
2015-09-09 16:40:43 +1000

    Test3
    Test commit

--- DavidsRealEstate/protected/models/Propertylisting.php
+++ DavidsRealEstate/protected/models/Propertylisting.php
@@ -113,7 +113,7 @@ class Propertylisting extends CActiveRecord
 		$criteria->compare('address',$this->address,true);
 		$criteria->compare('rent',$this->rent,true);
 		$criteria->compare('rentfreq',$this->rentfreq,true);
-		$criteria->compare('numBathroom',$this->numBathroom, false, '<=');
+		$criteria->compare('numBathroom',$this->numBathroom, false, '<=:');
 		$criteria->compare('numBedroom',$this->numBedroom);
 		$criteria->compare('numCarPorts',$this->numCarPorts);
 		$criteria->compare('createTime',$this->createTime,true);
