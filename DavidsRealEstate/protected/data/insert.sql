6302c743795238a88d10686dd49f20fb81319eb3
jsrmitch <mitchsainty@gmail.com>
2015-10-13 22:40:10 +1000

    CRUD options for user accounts

--- DavidsRealEstate/protected/data/insert.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO tbl_lookup (name, type, code, position) VALUES ('Draft', 'PostStatus', 1, 1);
-INSERT INTO tbl_lookup (name, type, code, position) VALUES ('Published', 'PostStatus', 2, 2);
-INSERT INTO tbl_lookup (name, type, code, position) VALUES ('Archived', 'PostStatus', 3, 3);
